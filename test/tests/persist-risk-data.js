b5ec0cfd9371748f21850d30b55775d129d2950a
katyayani boggulapalli <kaboggulapalli@paypal.com>
2020-05-07 21:58:14 -0700

    save only cookies string in localstorage

--- test/tests/persist-risk-data.js
+++ test/tests/persist-risk-data.js
@@ -7,12 +7,15 @@ import { constants } from '../../src/data-collector/constants';
 
 
 describe('persist risk data cases', () => {
-    const cookiesData = 'sc_f=MyNj1ZMuHH7ycH-;Domain=c.paypal.com;Max-Age=157680000;Path=/;Secure;Version=1;Expires=Wed, UGZUWCKM6F_awXE8WyEURJrBYQG=NSZp9DW0Evk_i;Domain=.paypal.com;Max-Age=630720000;Path=/;Secure;Version=1;Expires=Thu';
-    persistRiskData(cookiesData);
+    const riskDataCookies = {
+        cookies:       'sc_f=MyNj1ZMuHH7ycH-;Domain=c.paypal.com;Max-Age=157680000;Path=/;Secure;Version=1;Expires=Wed, UGZUWCKM6F_awXE8WyEURJrBYQG=NSZp9DW0Evk_i;Domain=.paypal.com;Max-Age=630720000;Path=/;Secure;Version=1;Expires=Thu',
+        paypalDebugId: 3452354345345
+    };
+    persistRiskData(riskDataCookies);
     it('should successfully get cookies from localstorage ', () => {
         if (isLocalStorageEnabled()) {
-            if (cookiesData !== window.localStorage.getItem(constants.localStorageRiskData)) {
-                throw new Error(`Expected cookiesData to be ${ window.localStorage.getItem(constants.localStorageRiskData)  }, got ${ window.localStorage.getItem(constants.localStorageRiskData) }`);
+            if (riskDataCookies.cookies !== window.localStorage.getItem(constants.localStorageRiskData)) {
+                throw new Error(`Expected riskDataCookies to be ${ window.localStorage.getItem(constants.localStorageRiskData)  }, got ${ window.localStorage.getItem(constants.localStorageRiskData) }`);
             }
         }
     });
